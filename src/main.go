416120b463a2aec570bf51c378e14f05781251ac
Konata09 <lz9709@gmail.com>
2021-07-07 23:32:12 +0800

    add new api

--- src/main.go
+++ src/main.go
@@ -24,17 +24,42 @@ func initDBConn() {
 func main() {
 	initDBConn()
 	mux := http.NewServeMux()
-	mux.Handle("/api/v1/login", http.HandlerFunc(Login))
-	mux.Handle("/api/v1/refresh", http.HandlerFunc(RefreshToken))
-	mux.Handle("/api/v1/getCommand", VerifyHeader(http.HandlerFunc(GetCommand)))
-	mux.Handle("/api/v1/getDevice", VerifyHeader(http.HandlerFunc(GetDevice)))
-	mux.Handle("/api/v1/sendUDP", VerifyHeader(http.HandlerFunc(SendUDP)))
-	mux.Handle("/api/v1/sendWOL", VerifyHeader(http.HandlerFunc(SendWOL)))
-	mux.Handle("/api/v1/user/changePassword", VerifyHeader(http.HandlerFunc(UserChangePassword)))
-	mux.Handle("/api/v1/admin/changePassword", VerifyHeader(VerifyAdmin(http.HandlerFunc(AdminChangePassword))))
-	mux.Handle("/api/v1/admin/setUser", VerifyHeader(VerifyAdmin(http.HandlerFunc(SetUser))))
-	mux.Handle("/api/v1/admin/setCommand", VerifyHeader(VerifyAdmin(http.HandlerFunc(SetCommand))))
-	mux.Handle("/api/v1/admin/setDevice", VerifyHeader(VerifyAdmin(http.HandlerFunc(SetDevice))))
+	mux.Handle("/api/v2/login", http.HandlerFunc(Login))
+	mux.Handle("/api/v2/refresh", http.HandlerFunc(RefreshToken))
+	mux.Handle("/api/v2/logout", http.HandlerFunc())
+	//mux.Handle("/api/v2/getCommand", VerifyHeader(http.HandlerFunc(GetCommand)))
+	//mux.Handle("/api/v2/getDevice", VerifyHeader(http.HandlerFunc(GetDevice)))
+	//mux.Handle("/api/v2/sendUDP", VerifyHeader(http.HandlerFunc(SendUDP)))
+	//mux.Handle("/api/v2/sendWOL", VerifyHeader(http.HandlerFunc(SendWOL)))
+	mux.Handle("/api/v2/user/changePassword", VerifyHeader(http.HandlerFunc(UserChangePassword)))
+	mux.Handle("/api/v2/admin/changePassword", VerifyHeader(VerifyAdmin(http.HandlerFunc(AdminChangePassword))))
+	mux.Handle("/api/v2/admin/setUser", VerifyHeader(VerifyAdmin(http.HandlerFunc(SetUser))))
+	mux.Handle("/api/v2/admin/setCommand", VerifyHeader(VerifyAdmin(http.HandlerFunc(SetCommand))))
+	mux.Handle("/api/v2/admin/setDevice", VerifyHeader(VerifyAdmin(http.HandlerFunc(SetDevice))))
+	mux.Handle("/api/v2/user/changePhone",VerifyHeader(http.HandlerFunc()))
+	// 返回全部教室和分组 含基本状态
+	mux.Handle("/api/v2/getRooms", VerifyHeader(http.HandlerFunc()))
+	// 返回教室ping状态
+	mux.Handle("/api/v2/getRoomDetail", VerifyHeader(http.HandlerFunc()))
+	// 返回教室详情
+	mux.Handle("/api/v2/getRoomStatus", VerifyHeader(http.HandlerFunc()))
+	// put 修改教室设备的地址 名称 组
+	mux.Handle("/api/v2/admin/setRoom", VerifyHeader(VerifyAdmin(http.HandlerFunc())))
+	// 发送教室控制命令
+	mux.Handle("/api/v2/sendCmd",VerifyHeader(http.HandlerFunc()))
+	// 返回我的工单
+	mux.Handle("/api/v2/getMyTicket", VerifyHeader(http.HandlerFunc()))
+	// 返回全部工单 动态确定获取条数
+	mux.Handle("/api/v2/getTicket", VerifyHeader(http.HandlerFunc()))
+	// 返回工单详情
+	mux.Handle("/api/v2/getTicketDetail", VerifyHeader(http.HandlerFunc()))
+	mux.Handle("/api/v2/admin/setTicketDutyUser", VerifyHeader(VerifyAdmin(http.HandlerFunc())))
+	mux.Handle("/api/v2/deleteTicket", VerifyHeader(VerifyAdmin(http.HandlerFunc())))
+	mux.Handle("/api/v2/setTicketDone", VerifyHeader(http.HandlerFunc()))
+	// 修改值班表
+	mux.Handle("/api/v2/setDuty", VerifyHeader(VerifyAdmin(http.HandlerFunc())))
+	// 获得当日值班
+	mux.Handle("/api/v2/getDutyUser", VerifyHeader(http.HandlerFunc()))
 
 	//fmt.Println(getSubnetBroadcast("172.16.0.254",32))
 	fmt.Println("UDPServer listen on 63112")
